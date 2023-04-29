96fb37bb4f12e4980bea2e3c0c5e8dc1de458678
Carlos Ryan Macedo <cryan200215@gmail.com>
2023-04-29 20:02:29 -0300

    Criação da Entidade, Interface e lógica principal para o consumo da API de CEPs

---
+++ CepApi/CepApi/Program.cs
@@ -0,0 +1,27 @@
+﻿using CepApi;
+using Refit;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            var cepClient = RestService.For<ICepApiService>("http://viacep.com.br");
+            Console.WriteLine("Informe seu CEP:");
+
+            string cepInformado = Console.ReadLine().ToString();
+            Console.WriteLine("Consultando informações do CEP: " + cepInformado);
+
+            var address = await cepClient.GetAddressAsync(cepInformado);
+
+            Console.Write($"\nLogradouro:{address.Logradouro}\nBairro:{address.Bairro}" + $"\nEstado:{address.Uf}\nCódigo Ibge:{address.Ibge},\nComplemento:{address.Complemento}");
+            
+            Console.ReadKey();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Erro na consulta de cep:" +ex.Message);
+        }
+    }
+}
